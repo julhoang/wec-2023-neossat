29b49452eff304a81efd726327f8f9433a6403e4
Eduardo Szeckir <eszeckirjr@gmail.com>
2023-01-20 14:15:55 -0800

    initial structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+import { Land } from "@/utils/types";
+import { HStack, Heading, ButtonGroup, Button } from "@chakra-ui/react";
+
+const Header = ({
+  gridType,
+  setGridType,
+}: {
+  gridType: Land;
+  setGridType: (gridType: Land) => void;
+}) => {
+  return (
+    <HStack my={4} px={4} py={2} justify="space-between">
+      <Heading size="3xl">Explore BC</Heading>
+      <ButtonGroup isAttached>
+        <Button
+          size="lg"
+          fontSize="2xl"
+          bgColor="green.300"
+          fontWeight={gridType === "land" ? "bold" : "normal"}
+          onClick={() => setGridType("land")}
+        >
+          Land
+        </Button>
+        <Button
+          size="lg"
+          fontSize="2xl"
+          bgColor="blue.200"
+          fontWeight={gridType === "water" ? "bold" : "normal"}
+          onClick={() => setGridType("water")}
+        >
+          Water
+        </Button>
+        <Button
+          size="lg"
+          fontSize="2xl"
+          bgColor="gray.400"
+          fontWeight={gridType === "mountain" ? "bold" : "normal"}
+          onClick={() => setGridType("mountain")}
+        >
+          Mountain
+        </Button>
+      </ButtonGroup>
+    </HStack>
+  );
+};
+
+export default Header;

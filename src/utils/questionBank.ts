828b87eba9e7035232cc96dd7e9fd0c8893493f6
julhoang <64108232+julhoang@users.noreply.github.com>
2023-01-20 15:51:11 -0800

    Update question bank - count 14

--- src/utils/questionBank.ts
+++ src/utils/questionBank.ts
@@ -1,15 +1,17 @@
 import { Land, Question } from "./types";
 
+// Current question count: 14
+
 export const questionBank: Question[] = [
   {
-    question: "What is the largest city in British Columbia?",
+    question: "What is the largest city in British Columbia (by area)?",
     options: [
       { id: 1, text: "Victoria" },
       { id: 2, text: "Kelowna" },
       { id: 3, text: "Vancouver" },
-      { id: 4, text: "Whistler" },
+      { id: 4, text: "Abbotsford" },
     ],
-    correct: 0,
+    correct: 4,
     theme: "land",
     done: false,
   },
@@ -21,31 +23,7 @@ export const questionBank: Question[] = [
       { id: 3, text: "12" },
       { id: 4, text: "15" },
     ],
-    correct: 0,
-    theme: "land",
-    done: false,
-  },
-  {
-    question: "What is the name of the oldest known Western Red Cedar tree in British Columbia?",
-    options: [
-      { id: 1, text: "The Hanging Garden Tree" },
-      { id: 2, text: "The Ancient Cedar Tree" },
-      { id: 3, text: "The Ancient Cedar Tree" },
-      { id: 4, text: "The Ancient Cedar Tree" },
-    ],
-    correct: 0,
-    theme: "land",
-    done: false,
-  },
-  {
-    question: "What is the name of the oldest known Western Red Cedar tree in British Columbia?",
-    options: [
-      { id: 1, text: "The Hanging Garden Tree" },
-      { id: 2, text: "The Ancient Cedar Tree" },
-      { id: 3, text: "The Ancient Cedar Tree" },
-      { id: 4, text: "The Ancient Cedar Tree" },
-    ],
-    correct: 0,
+    correct: 1,
     theme: "land",
     done: false,
   },
@@ -62,14 +40,14 @@ export const questionBank: Question[] = [
     done: false,
   },
   {
-    question: "What is the highest population of endangered Mountain Caribou found in Canada?",
+    question: "Name of a national park in British Columbia?",
     options: [
-      { id: 1, text: "Alberta" },
-      { id: 2, text: "Saskatchewan" },
-      { id: 3, text: "Manitoba" },
-      { id: 4, text: "British Columbia" },
+      { id: 1, text: "Point Pelee" },
+      { id: 2, text: "Yoho" },
+      { id: 3, text: "Frontenac" },
+      { id: 4, text: "Atikaki" },
     ],
-    correct: 4,
+    correct: 2,
     theme: "land",
     done: false,
   },
@@ -94,19 +72,7 @@ export const questionBank: Question[] = [
       { id: 3, text: "Roosevelt Elk" },
       { id: 4, text: "Beaver" },
     ],
-    correct: 3,
-    theme: "land",
-    done: false,
-  },
-  {
-    question: "What is the highest population of grizzly bears found in Canada?",
-    options: [
-      { id: 1, text: "Alberta" },
-      { id: 2, text: "Saskatchewan" },
-      { id: 3, text: "Manitoba" },
-      { id: 4, text: "British Columbia" },
-    ],
-    correct: 4,
+    correct: 1,
     theme: "land",
     done: false,
   },
@@ -120,6 +86,92 @@ export const questionBank: Question[] = [
     ],
     correct: 1,
     theme: "land",
+    done: false,
+  },
+  {
+    question: "How long is the coastline in BC?",
+    options: [
+      { id: 1, text: "Under 5,000 km" },
+      { id: 2, text: "5,000 - 12,000 km" },
+      { id: 3, text: "12,000 - 24,000 km" },
+      { id: 4, text: "Over 24,000 km" },
+    ],
+    correct: 4,
+    theme: "water",
+    done: false,
+  },
+
+  {
+    question: "British Columbia is home to the world’s largest population of ...",
+    options: [
+      { id: 1, text: "wild Pacific salmon" },
+      { id: 2, text: "Bull Trout" },
+      { id: 3, text: "Gray Whale" },
+      { id: 4, text: "White Sturgeon" },
+    ],
+    correct: 1,
+    theme: "water",
+    done: false,
+  },
+  {
+    question:
+      "... is one of the most biologically productive habitats in the world, and is home to many endangered species.",
+    options: [
+      { id: 1, text: "Robson Bight" },
+      { id: 2, text: "Salish Sea" },
+      { id: 3, text: "Georgia Strait" },
+      { id: 4, text: "Skagit River Forest" },
+    ],
+    correct: 3,
+    theme: "land",
+    done: false,
+  },
+  {
+    question: "Name the highest mountain in British Columbia",
+    options: [
+      { id: 1, text: "King Peak" },
+      { id: 2, text: "Mount Saint Elias" },
+      { id: 3, text: "Mount Logan" },
+      { id: 4, text: "Mount Waddington" },
+    ],
+    correct: 3,
+    theme: "mountain",
+    done: false,
+  },
+  {
+    question: "Which mountain in BC has the highest peak",
+    options: [
+      { id: 1, text: "Fairweather Mountain" },
+      { id: 2, text: "King Peak" },
+      { id: 3, text: "Mount Saint Elias" },
+      { id: 4, text: "Mount Waddington" },
+    ],
+    correct: 1,
+    theme: "mountain",
+    done: false,
+  },
+  {
+    question: "Which national park has the highest mountain peak in BC?",
+    options: [
+      { id: 1, text: "Mount Robson Provincial Park" },
+      { id: 2, text: "Glacier Bay National Park and Preserve" },
+      { id: 3, text: "Yoho National Park" },
+      { id: 4, text: "Kootenay National Park" },
+    ],
+    correct: 2,
+    theme: "mountain",
+    done: false,
+  },
+  {
+    question: "How many named mountain peaks are there in British Columbia?",
+    options: [
+      { id: 1, text: "5908" },
+      { id: 2, text: "6745" },
+      { id: 3, text: "7996" },
+      { id: 4, text: "9041" },
+    ],
+    correct: 3,
+    theme: "mountain",
     done: false,
   },
 ];
